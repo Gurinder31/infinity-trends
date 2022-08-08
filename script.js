a207411b022cb5b4a0e339b6954491cfa714e457
Gurinder31 <109625259+Gurinder31@users.noreply.github.com>
2022-08-08 21:28:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $('.category-wrap').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
+    });
+
+})
